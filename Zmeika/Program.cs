4312bd50d7e863286df386ccc511e408f9e3f360
IvanZuzichev <isip_i.s.zuzichev@mpt.ru>
2022-12-08 23:40:24 +0300

    Добавьте файлы проекта.

---
+++ Zmeika/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Zmeika
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // перенос в код
+            Snake snake = new Snake();
+            while(true)
+            {
+                snake.Gran();
+                snake.retr();
+                snake.kvadrat();
+             }
+        }
+        internal void Finaly()
+        {
+            // возврат доделать в ИС 2 Х
+            // само в себя Х
+            Console.Clear();
+            Console.Write("Конец игры");
+        }
+    }
+}
